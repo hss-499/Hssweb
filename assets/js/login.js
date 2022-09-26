3abcf686bf8e07ffb72109ae869c3d06ad020091
hss <hss_499@163.com>
2022-09-26 17:23:31 +0800

    简单绘制了一下

---
+++ assets/js/login.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('#go2Reg').on('click',function(){
+        $('.login-wrap').hide()
+        $('.reg-wrap').show()
+    })
+
+    $('#go2Longin').on('click',function(){
+        $('.reg-wrap').hide()
+        $('.login-wrap').show()
+
+    })
+
+
+
+})
