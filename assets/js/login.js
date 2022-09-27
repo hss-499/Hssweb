bfaf37d04b7fa3467e6851175d8a481d3d6fe3ba
hss <hss_499@163.com>
2022-09-27 15:16:30 +0800

    提交了登录事件

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,7 +9,12 @@ $(function(){
         $('.login-wrap').show()
 
     })
+  
 
-
-
+    //从layui
+    let form =layui.form
+    // 通过form.verify()
+    form.verify({
+        pwd:[ /^[\S]{6,12}$/ ,'密码必须6到12位，且不能出现空格'] 
+    })
 })
